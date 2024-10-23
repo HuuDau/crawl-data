258f284bb5d17808d39d6eb8cd59c45a96c30e35
Nguyen Dau <kgvenusbn@gmail.com>
2024-10-23 14:38:38 +0700

    update pup

---
+++ 15_linh_vuc/2BA/createFIle.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const arr = require('./2BA.json')
+
+    function createJsonFiles(array) {
+        array.forEach(item => {
+          const fileName = `${item}.json`; // Tên tệp tương ứng
+          const data = []; // Dữ liệu mẫu
+      
+          // Ghi dữ liệu vào tệp JSON
+          fs.writeFile(fileName, JSON.stringify(data, null, 2), (err) => {
+            if (err) {
+              console.error(`Lỗi khi tạo tệp ${fileName}:`, err);
+            } else {
+              console.log(`Đã tạo tệp: ${fileName}`);
+            }
+          });
+        });
+      }
+      
+      createJsonFiles(arr);
