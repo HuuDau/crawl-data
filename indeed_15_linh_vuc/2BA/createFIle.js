7d1367294debf5538fdd8dedd8dfeb9d32b21883
Nguyen Dau <kgvenusbn@gmail.com>
2024-10-30 17:57:16 +0700

    update: data crawl indeed

---
+++ indeed_15_linh_vuc/2BA/createFIle.js
@@ -0,0 +1,221 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+const fss = require('fs');
+const fs = require('fs').promises;
+const dayjs = require('dayjs');
+
+const crawData = async (payload) => {
+  try {
+    const encodedString = encodeURIComponent(payload).replace(/%20/g, '+')
+    console.log(encodedString)
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    let listOriginUrlKeySearch = []
+
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36');
+      await page.goto(`https://jobs.vn.indeed.com/jobs?q=${encodedString}`);
+      await new Promise(resolve => setTimeout(resolve, 30000));
+      let hasNextPage = await page.$('a[data-testid="pagination-page-next"]') !== null;
+  if (hasNextPage) {
+    console.log("Có phần tử với data-testid='pagination-page-next'");
+  } else {
+    console.log("Không tìm thấy phần tử với data-testid='pagination-page-next'");
+  }
+  while (hasNextPage) {
+    const listLink = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('.jcs-JobTitle.css-1baag51.eu4oa1w0')).map(link => link.href);
+    });
+  
+    listOriginUrlKeySearch = [...listOriginUrlKeySearch, ...listLink];
+  
+    hasNextPage = await page.$('a[data-testid="pagination-page-next"]') !== null;
+    if (hasNextPage) {
+      await page.click('a[data-testid="pagination-page-next"]');
+      await page.waitForNavigation({ waitUntil: 'networkidle2' }); 
+    }
+  }
+
+
+      const extractJobDetails = async (link) => {
+        await page.goto(link)
+        const datas = await page.evaluate((link,today) => {
+      
+          const companyUrl = ()=>{
+            const url = document.querySelector('.css-1ioi40n.e19afand0');
+            return url?.href || undefined
+          }
+    
+          const description = () => {
+            const div = document.querySelector('#jobDescriptionText');
+            if (div) {
+              return Array.from(div.children).map(child => child?.innerText.replaceAll('\n', ' '));
+            } else {
+              return [];
+            }
+          };
+    
+          const workPlace  =()=>{
+            const locationElement = Array.from(document.querySelectorAll("b"))
+                .find(b => b.innerText.trim() === "Địa điểm làm việc:");
+            if (locationElement && locationElement.nextSibling) {
+                return locationElement.nextSibling.textContent.trim();
+            }
+            return null;
+          }
+          const luong  =()=>{
+            const locationElement = Array.from(document.querySelectorAll("b"))
+                .find(b => b.innerText.trim() === "Lương:");
+            if (locationElement && locationElement?.nextSibling) {
+                return locationElement.nextSibling?.textContent.trim();
+            }
+            return null;
+          }
+          
+          const linhVuc = ()=>{
+            const locationElement = Array.from(document.querySelectorAll("b"))
+                .find(b => b.innerText.trim() === "Lĩnh vực:");
+            if (locationElement && locationElement?.nextSibling) {
+                return locationElement.nextSibling?.textContent.trim();
+            }
+            return null;
+          }
+           
+          const members = ()=>{
+            const locationElement = Array.from(document.querySelectorAll("b"))
+                .find(b => b.innerText.trim() === "Số lượng cần tuyển:");
+            if (locationElement && locationElement?.nextSibling) {
+                return locationElement.nextSibling?.textContent.trim();
+            }
+            return null;
+          }
+    
+          const benefit = ()=>{
+            const locationElement = Array.from(document.querySelectorAll("b"))
+                .find(b => b.innerText.trim() === "Quyền lợi:");
+            if (locationElement && locationElement?.nextSibling) {
+                return locationElement.nextSibling?.textContent.trim();
+            }
+            return null;
+    
+          }
+          
+          const otherBenefit = ()=>{
+            const jobRequirementElement = Array.from(document.querySelectorAll("b"))
+            .find(b => b?.innerText.trim() === "Other Benefits:");
+    
+        if (jobRequirementElement) {
+            const ulElement = jobRequirementElement.nextElementSibling?.nextElementSibling;
+            if (ulElement && ulElement?.tagName === "UL") {
+                return Array.from(ulElement.querySelectorAll("li")).map(li => li?.innerText.trim());
+            }
+        }
+    
+        return null;
+          }
+    
+          const title = () => {
+            const div = document.querySelector('h1');
+              return Array.from(div.children).map(child => child?.innerText);
+          };
+          const salary_range = ()=>{
+            // 
+            const elements = document.querySelectorAll('.js-match-insights-provider-e6s05i.eu4oa1w0');
+            const texts = Array.from(elements).map(element => element?.innerText);
+            const target = texts.find((text)=> text.includes(`Pay\n`))
+            return  target ? target.replace('Pay\n','')  : undefined;
+          }
+          const degree = ()=>{
+            const locationElement = Array.from(document.querySelectorAll("b"))
+            .find(b => b.innerText.trim() === "Bằng cấp:");
+            if (locationElement && locationElement?.nextSibling) {
+                return locationElement.nextSibling?.textContent.trim();
+            }
+            return null;
+              }
+    
+              const remoteJob = ()=>{
+                const locationElement = Array.from(document.querySelectorAll("b"))
+                .find(b => b.innerText.trim() === "Loại hình công việc:");
+                if (locationElement && locationElement?.nextSibling) {
+                    return locationElement.nextSibling?.textContent.trim();
+                }
+                return null;
+                  }
+    
+                  const yc = ()=>{
+                    const jobRequirementElement = Array.from(document.querySelectorAll("b"))
+                    .find(b => b?.innerText.trim() === "Yêu cầu công việc :");
+                
+                if (jobRequirementElement) {
+                    const ulElement = jobRequirementElement.nextElementSibling?.nextElementSibling;
+                    if (ulElement && ulElement?.tagName === "UL") {
+                        return Array.from(ulElement.querySelectorAll("li")).map(li => li?.innerText.trim());
+                    }
+                }
+        
+                return null;
+                      }
+    
+          return {
+            title: title()[0],
+            urlCompany:companyUrl(),
+            description: description(),
+            crawlTime: today,
+            salary_range:salary_range(),
+            local:workPlace(),
+            salary:luong(),
+            linhVuc:linhVuc(),
+            members:members(),
+            benefit:benefit(),
+            degree:degree(),
+            remoteJob:remoteJob(),
+            linkJob:link,
+            yc:yc(),
+            otherBenefit:otherBenefit(),
+    
+          };
+        }, link,dayjs().format('DD/MM/YYYY'));
+      return datas
+      };
+      const results =[]
+   
+      for(const link of listOriginUrlKeySearch){
+        const data = await extractJobDetails(link);
+        results.push(data)
+        await new Promise(resolve => setTimeout(resolve, 3000));
+      }
+      
+      fss.writeFileSync(`${payload}.json`, JSON.stringify(results, null, 2));
+      console.log(`Dữ liệu đã được lưu vào ${payload}.json`);
+    await browser.close();
+  } catch (error) {
+    console.error('Lỗi:', error);
+
+  }
+};
+
+
+
+
+const getListFileName = async () => {
+  try {
+    const files = await fs.readdir('.');
+    const data = files.filter(name => name !== '2BA.json' && name !== 'createFIle.js' && name !== 'index.js');
+    const listKeysearch = data.map((name) => name.slice(0, name.length - 5))
+    return listKeysearch
+  } catch (err) {
+    throw err;
+  }
+}
+
+getListFileName()
+  .then(async (keySearch) => {
+    for (const payload of keySearch) {
+      await crawData(payload.trim())
+      await new Promise(resolve => setTimeout(resolve, 20000));
+    }
+  })
+  .catch(err => console.error(err));
+
---
+++ topdev_15_linh_vuc/8e/createFIle.js
@@ -0,0 +1,88 @@
+
+
+
+const arr = require('./8e.json')
+
+const fs = require('fs').promises;
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+const fss = require('fs');
+const axios = require('axios') ;
+const objJwt = require('../token.json')
+
+const getListFileName = async () => {
+  try {
+    const files = await fs.readdir('.'); 
+    return files.filter(name => name !== '8e.json' && name !== 'createFIle.js' && name !== 'index.js'); 
+  } catch (err) {
+    throw err; 
+  }
+}
+
+const getData = async (payload)=>{
+  try {
+
+    const response = await axios.get('https://api.topdev.vn/td/v2/suggested-units/', {
+      params: {
+        'keyword': payload.fileName.slice(0,payload.fileName.length-5),
+        'locale': 'vi_VN'
+      },
+      headers: {
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        'cookie': objJwt.cookie,
+        'origin': 'https://topdev.vn',
+        'pragma': 'no-cache',
+        'priority': 'u=1, i',
+        'referer': 'https://topdev.vn/',
+        'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+       'x-xsrf-token': objJwt.token
+      }
+    });
+  
+    const data=response?.data?.data
+    if(data.length > 0){
+      let listData = []
+      for(const item of data){
+        const result = await axios.get(`https://api.topdev.vn/td/v2/jobs?keyword=&region_ids=&skills_id=${item.id}&industries_ids=&job_levels_ids=&job_types_ids=&contract_types_ids=&salary_range=&experiences_id=&ordering=newest_job&company=&_f=50696&page=1&page_size=${objJwt.size}&locale=vi_VN&fields[job]=id,title,salary,slug,company,extra_skills,skills_str,skills_arr,skills_ids,job_types_str,job_levels_str,job_levels_arr,job_levels_ids,addresses,status_display,detail_url,job_url,salary,published,refreshed,applied,candidate,requirements_arr,packages,benefits,content,features,contract_types_ids,is_free,is_basic,is_basic_plus,is_distinction&fields[company]=tagline,addresses,skills_arr,industries_arr,industries_ids,industries_str,image_cover,image_galleries,num_job_openings,company_size,nationalities_str,skills_str,skills_ids,benefits,num_employees`,{
+          headers: {
+            'Cache-Control': 'no-cache',
+              cookie:objJwt.cookie
+          }})
+     
+          listData=[...listData,...result?.data?.data]
+       
+      }
+      fss.writeFileSync(`${payload?.fileName}`, JSON.stringify(listData, null, 2));
+      console.log(`Dữ liệu đã được lưu vào ${payload?.fileName}`);
+    
+    }else{
+      console.log(`key search ${payload?.fileName} has no data`);
+    }
+   
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+  getListFileName().then(async (data) => {
+    const payloadArr = data.map((name)=>{
+       return {
+         fileName:name,
+       }
+     })
+ 
+     for (const payload of payloadArr) {
+        await getData(payload)
+       await new Promise(resolve => setTimeout(resolve, 1000));
+     }
+   }) 
+   .catch(err => console.error(err));
